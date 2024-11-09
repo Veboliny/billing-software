7e591a160f099ee04f8e80dfd5aff14d4b968bb6
Bxota <thomas.pro.leterme@gmail.com>
2024-11-09 19:51:43 +0100

    On change les frameworks

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.vebo-factu.app',
+  appName: 'my-capacitor-app',
+  webDir: 'dist'
+};
+
+export default config;
